0f2f8782720b8f89e4a78b6a6a02a2a1605bbf55
alimirza09 <106017617+alimirza09@users.noreply.github.com>
2026-04-23 20:45:46 +0500

    added db.rs

---
+++ src/package/db.rs
@@ -0,0 +1,70 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
+
+const DB_PATH: &str = "/var/lib/thanatos/db.json";
+
+fn get_db_path() -> PathBuf {
+    std::env::var("THANATOS_DB_PATH")
+        .map(PathBuf::from)
+        .unwrap_or_else(|_| PathBuf::from(DB_PATH))
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct InstalledPackage {
+    pub name: String,
+    pub version: String,
+    pub release: u32,
+    pub depends: Vec<String>,
+    pub files: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Default)]
+pub struct PackageDb {
+    packages: HashMap<String, InstalledPackage>,
+}
+
+impl PackageDb {
+    pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
+        let path = get_db_path();
+
+        if !path.exists() {
+            return Ok(PackageDb::default());
+        }
+
+        let content = fs::read_to_string(path)?;
+        Ok(serde_json::from_str(&content)?)
+    }
+
+    pub fn save(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let path = get_db_path();
+
+        if let Some(parent) = path.parent() {
+            fs::create_dir_all(parent)?;
+        }
+
+        fs::write(path, serde_json::to_string_pretty(self)?)?;
+        Ok(())
+    }
+
+    pub fn insert(&mut self, pkg: InstalledPackage) {
+        self.packages.insert(pkg.name.clone(), pkg);
+    }
+
+    pub fn remove(&mut self, name: &str) -> Option<InstalledPackage> {
+        self.packages.remove(name)
+    }
+
+    pub fn get(&self, name: &str) -> Option<&InstalledPackage> {
+        self.packages.get(name)
+    }
+
+    pub fn is_installed(&self, name: &str) -> bool {
+        self.packages.contains_key(name)
+    }
+
+    pub fn all(&self) -> Vec<&InstalledPackage> {
+        self.packages.values().collect()
+    }
+}
