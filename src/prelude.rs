cd08e815280471378a49583a00c0c3e5281ba0dc
alimirza09 <106017617+alimirza09@users.noreply.github.com>
2026-07-04 14:07:28 +0500

    added prelude

---
+++ src/prelude.rs
@@ -0,0 +1,5 @@
+pub use crate::System;
+pub use crate::access_control::group::Group;
+pub use crate::access_control::user::{Password, Shell, User};
+pub use crate::package::Package;
+pub use crate::package::db::InstallReason;
