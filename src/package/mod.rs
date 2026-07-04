08e9d2d4c26b1e13e71745bc6869a1193a4dd0f1
alimirza09 <106017617+alimirza09@users.noreply.github.com>
2026-07-04 14:07:50 +0500

    fixed warnings

--- src/package/mod.rs
+++ src/package/mod.rs
@@ -2,4 +2,4 @@ mod build_file;
 pub mod db;
 
 pub use self::build_file::{fetch_build_file, parse_pkgbuild};
-pub use self::db::{InstallReason, InstallState, Package, PackageDb, PackageFormat};
+pub use self::db::{Package, PackageFormat};
